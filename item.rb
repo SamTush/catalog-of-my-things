09ebc252608face1be39e92e8ef0089f646c3054
lengushuru charles <charleslengushuru@gmail.com>
2023-06-06 13:30:14 +0300

    Fix Linter error.

--- item.rb
+++ item.rb
@@ -1,27 +1,22 @@
 require 'Date'
-class  Item
-
+class Item
   attr_accessor :genre, :author, :source, :label
 
-  def initialize(id,publish_date,archive:false)
+  def initialize(id, publish_date, archive: false)
     @id = id
     @publish_date = publish_date
     @archive = archive
   end
 
   def move_to_archive()
-    if can_be_archived?
-      @archive = true
-
+    @archive = true if can_be_archived?
   end
 
   private
+
   def can_be_archived?
-    if Date.today << 120 > @publish_date
-      return true
-    end
-    return false
+    return true if Date.today << 120 > @publish_date
+
+    false
   end
 end
-
-puts Date.today << 120
