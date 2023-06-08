9012453510932c84a45c21e98ebe1f815c563902
lengushuru charles <charleslengushuru@gmail.com>
2023-06-08 15:39:11 +0300

    Update game.rb file

--- game.rb
+++ game.rb
@@ -1,17 +1,51 @@
-require 'date'
 require_relative 'item'
+require 'date'
 
 class Game < Item
-  attr_accessor :last_played_at, :multiplayer
+  attr_accessor :multiplayer, :last_played_at, :publish_date, :authors
+  attr_reader :id, :title
 
-  def initialize(id, publish_date, last_played_at, multiplayer,archive: false)
-    super(id, publish_date, archive: archive)
-    @multiplayer  =multiplayer
+  def initialize(title, multiplayer, last_played_at, publish_date, authors = [])
+    @id = Random.rand(1..1000)
+    super()
+    @title = title
+    @multiplayer = multiplayer
     @last_played_at = last_played_at
+    @publish_date = Date.parse(publish_date).strftime('%Y/%m/%d')
+    @can_be_archived = can_be_archived?
+    @authors = []
+    add_authors(authors)
+  end
+
+  def self.json_create(object)
+    new(object['title'], object['multiplayer'], object['last_played_at'], object['publish_date'], object['authors'])
+  end
+
+  def add_authors(authors)
+    authors.each { |author| add_author(author) }
+  end
+
+  def add_author(author)
+    return if authors.include?(author)
+
+    authors << author
+    author.add_item(self)
   end
 
   def can_be_archived?
-    super && @last_played_at < Date.today - 2*365
+    super || (Date.today - Date.parse(@last_played_at) > 365 * 2)
+  end
+
+  private :can_be_archived?
+
+  def to_hash
+    {
+      id: @id,
+      title: @title,
+      multiplayer: @multiplayer,
+      last_played_at: @last_played_at,
+      publish_date: @publish_date,
+      author_ids: @authors.map(&:id)
+    }
   end
 end
-
