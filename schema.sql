0c104f7aecbf502185557c4d3dca04b2f0b4cd78
Samson Tush <samo4421@icloud.com>
2023-06-09 17:11:48 +0300

    Add schema

--- schema.sql
+++ schema.sql
@@ -1,20 +1,44 @@
-CREATE TABLE books (
+-- Create Item Table--
+CREATE TABLE Item (
   id INT PRIMARY KEY,
-  genre VARCHAR(255),
-  author VARCHAR(255),
-  source VARCHAR(255),
-  label_id INT,
+  genre VARCHAR(200),
+  author VARCHAR(200),
+  source VARCHAR(200),
+  label VARCHAR(200),
   publish_date DATE,
-  publisher VARCHAR(255),
-  cover_state VARCHAR(255),
-  archived BOOLEAN,
-  FOREIGN KEY (label_id) REFERENCES labels(id)
+  archived BOOLEAN DEFAULT false,
+  can_be_archived BOOLEAN,
+  move_to_archive BOOLEAN,
+  add_genre INT REFERENCES Genre(id),
+  add_source INT REFERENCES Source(id),
+  add_author INT REFERENCES Author(id),
+  add_label INT REFERENCES Label(id)
 );
 
-CREATE TABLE labels (
-  id INT PRIMARY KEY,
-  name VARCHAR(255)
+--Create games table--
+CREATE TABLE game(
+  id INT GENERATED ALWAYS AS
+  IDENTITY PRIMARY KEY,
+  publish_date DATE,
+  multiplayer VARCHAR(200),
+  last_played_at DATE,
+  author_id INT REFERENCES author(id)
 );
+
+--Create authors table--
+CREATE TABLE author(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  first_name VARCHAR(200),
+  last_name VARCHAR(200)
+);
+
+--create genre table--
+CREATE TABLE genre(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(200)
+);
+
+--music_album table--
 
 CREATE TABLE musicalbum (
     id INT GENERATED ALWAYS AS IDENTITY,
@@ -25,3 +49,23 @@ CREATE TABLE musicalbum (
     PRIMARY KEY(id),
     CONSTRAINT fk_genre FOREIGN KEY(genre_id) REFERENCES genre(id)
 );
+
+--book table--
+
+CREATE TABLE  book (
+  id INT PRIMARY KEY REFERENCES item(id),
+  publisher VARCHAR(40),
+  cover_state VARCHAR(30)
+);
+
+--label table--
+
+CREATE TABLE label (
+  id INT PRIMARY KEY,
+  title VARCHAR(30),
+  color VARCHAR(20)
+);
+
+CREATE INDEX idx_games_author_id ON games (author_id);
+CREATE INDEX idx_musicalbum_genre_id ON musicalbum (genre_id);
+CREATE INDEX idx_book_label_id ON book (label_id);
